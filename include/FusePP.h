c97a2fe93c765fd91b31f2f3246a695ed508b596
Anh-Duy Vu <dvu4@ford.com>
2020-12-13 23:21:08 -0500

    add missing callbacks in fuse_operation

--- include/FusePP.h
+++ include/FusePP.h
@@ -35,11 +35,11 @@ namespace io::github::vuanhduy::libfusepp {
 class FusePP {
 public:
     FusePP();
-    FusePP(const FusePP&) = delete;
-    FusePP(const FusePP&&) = delete;
+    FusePP (const FusePP&) = delete;
+    FusePP (const FusePP&&) = delete;
 
-    FusePP& operator=(const FusePP&) = delete;
-    FusePP& operator=(const FusePP&&) = delete;
+    FusePP& operator= (const FusePP&) = delete;
+    FusePP& operator= (const FusePP&&) = delete;
 
     int launch(int argc, char *argv[]);
 
@@ -47,25 +47,171 @@ public:
         return -ENOSYS;
     };
 
-    virtual int readdir(const string &, void *, fuse_fill_dir_t, off_t, struct fuse_file_info *,
-                        enum fuse_readdir_flags) {
+    virtual int readlink (const string &, char *, size_t) {
         return -ENOSYS;
     };
 
-    virtual int create(const string &, mode_t, struct fuse_file_info *) {
+    virtual int mknod (const string &,  mode_t, dev_t) {
         return -ENOSYS;
     };
 
-    virtual int open(const string &, struct fuse_file_info *) {
+    virtual int mkdir (const string &,  mode_t) {
         return -ENOSYS;
     };
 
-    virtual int read(const string &, char *, size_t, off_t, fuse_file_info *) {
+    virtual int unlink (const char *) {
         return -ENOSYS;
     };
 
-    virtual int write(const string &, const char *, size_t,off_t, struct fuse_file_info *) {
+    virtual int rmdir (const char *) {
         return -ENOSYS;
+    };
+
+    virtual int symlink (const string &,  const char *) {
+        return -ENOSYS;
+    };
+
+    virtual int rename (const string &,  const char *, unsigned int) {
+        return -ENOSYS;
+    };
+
+    virtual int link (const string &,  const char *) {
+        return -ENOSYS;
+    };
+
+    virtual int chmod (const string &,  mode_t, struct fuse_file_info *) {
+        return -ENOSYS;
+    };
+
+    virtual int chown (const string &,  uid_t, gid_t, struct fuse_file_info *) {
+        return -ENOSYS;
+    };
+
+    virtual int truncate (const string &,  off_t, struct fuse_file_info *) {
+        return -ENOSYS;
+    };
+
+    virtual int open (const string &, struct fuse_file_info *) {
+        return -ENOSYS;
+    };
+
+    virtual int read (const string &, char *, size_t, off_t, fuse_file_info *) {
+        return -ENOSYS;
+    };
+
+    virtual int write (const string &, const char *, size_t,off_t, struct fuse_file_info *) {
+        return -ENOSYS;
+    };
+
+    virtual int statfs (const string &, struct statvfs *) {
+        return -ENOSYS;
+    };
+
+    virtual int flush (const string &, struct fuse_file_info *) {
+        return -ENOSYS;
+    };
+
+    virtual int release (const string &,  struct fuse_file_info *) {
+        return -ENOSYS;
+    };
+
+    virtual int fsync (const string &,  int, struct fuse_file_info *) {
+        return -ENOSYS;
+    };
+
+    virtual int setxattr (const string &,  const char *, const char *, size_t, int) {
+        return -ENOSYS;
+    };
+
+    virtual int getxattr (const string &,  const char *, char *, size_t) {
+        return -ENOSYS;
+    };
+
+    virtual int listxattr (const string &,  char *, size_t) {
+        return -ENOSYS;
+    };
+
+    virtual int removexattr (const string &,  const char *) {
+        return -ENOSYS;
+    };
+
+    virtual int opendir (const string &,  struct fuse_file_info *) {
+        return -ENOSYS;
+    };
+
+    virtual int readdir(const string &,  void *, fuse_fill_dir_t, off_t, struct fuse_file_info *, enum fuse_readdir_flags) {
+        return -ENOSYS;
+    };
+
+    virtual int releasedir (const string &,  struct fuse_file_info *) {
+        return -ENOSYS;
+    };
+
+    virtual int fsyncdir (const string &,  int, struct fuse_file_info *) {
+        return -ENOSYS;
+    };
+
+    virtual void *init (struct fuse_conn_info *, struct fuse_config *) {
+        return nullptr;
+    };
+
+    virtual void destroy (void *) {};
+
+    virtual int access (const string &, int) {
+        return -ENOSYS;
+    };
+
+    virtual int create (const string &, mode_t, struct fuse_file_info *) {
+        return -ENOSYS;
+    };
+
+    virtual int lock (const string &, struct fuse_file_info *, int, struct flock *) {
+        return -ENOSYS;
+    };
+
+    virtual int utimens (const string &, const struct timespec [2], struct fuse_file_info *) {
+        return -ENOSYS;
+    };
+
+    virtual int bmap (const string &,  size_t, uint64_t *) {
+        return -ENOSYS;
+    };
+
+#if FUSE_USE_VERSION < 35
+    virtual int ioctl (const string &,  int, void *, struct fuse_file_info *, unsigned int, void *) {
+#else
+    virtual int ioctl (const string &,  unsigned int, void *, struct fuse_file_info *, unsigned int, void *) {
+#endif
+        return -ENOSYS;
+    };
+
+    virtual int poll (const string &,  struct fuse_file_info *, struct fuse_pollhandle *, unsigned *) {
+        return -ENOSYS;
+    };
+
+    virtual int write_buf (const string &,  struct fuse_bufvec *, off_t, struct fuse_file_info *) {
+        return -ENOSYS;
+    };
+
+    virtual int read_buf (const string &,  struct fuse_bufvec **, size_t, off_t, struct fuse_file_info *) {
+        return -ENOSYS;
+    };
+
+    virtual int flock (const string &,  struct fuse_file_info *, int) {
+        return -ENOSYS;
+    };
+
+    virtual int fallocate (const string &,  int, off_t, off_t, struct fuse_file_info *) {
+        return -ENOSYS;
+    };
+
+    virtual ssize_t copy_file_range (const string &,  struct fuse_file_info *, off_t, const char *,
+                                     struct fuse_file_info *, off_t, size_t, int) {
+        return -ENOSYS;
+    };
+
+    virtual off_t lseek_cb (const string &,  off_t, int, struct fuse_file_info *) {
+      return -ENOSYS;
     };
 
 private:
@@ -75,13 +221,53 @@ private:
     /*
      * fuse_operations callbacks.
      */
-    static int getattr_cb(const char *path, struct stat *buf, struct fuse_file_info *fi);
-    static int readdir_cb(const char *path, void *buf, fuse_fill_dir_t filler, off_t off, struct fuse_file_info *fi,
-                          enum fuse_readdir_flags flags);
-    static int create_cb(const char *path, mode_t mode, struct fuse_file_info *fi);
-    static int open_cb(const char *path, struct fuse_file_info *fi);
-    static int read_cb(const char *path, char *buf, size_t size, off_t off, fuse_file_info *fi);
-    static int write_cb(const char *path, const char *buf, size_t size, off_t off, struct fuse_file_info *fi);
+    static int getattr_cb (const char *, struct stat *, struct fuse_file_info *);
+    static int readlink_cb (const char *, char *, size_t);
+    static int mknod_cb (const char *, mode_t, dev_t);
+    static int mkdir_cb (const char *, mode_t);
+    static int unlink_cb (const char *);
+    static int rmdir_cb (const char *);
+    static int symlink_cb (const char *, const char *);
+    static int rename_cb (const char *, const char *, unsigned int);
+    static int link_cb (const char *, const char *);
+    static int chmod_cb (const char *, mode_t, struct fuse_file_info *);
+    static int chown_cb (const char *, uid_t, gid_t, struct fuse_file_info *);
+    static int truncate_cb (const char *, off_t, struct fuse_file_info *);
+    static int open_cb (const char *, struct fuse_file_info *);
+    static int read_cb (const char *, char *, size_t, off_t, struct fuse_file_info *);
+    static int write_cb (const char *, const char *, size_t, off_t, struct fuse_file_info *);
+    static int statfs_cb (const char *, struct statvfs *);
+    static int flush_cb (const char *, struct fuse_file_info *);
+    static int release_cb (const char *, struct fuse_file_info *);
+    static int fsync_cb (const char *, int, struct fuse_file_info *);
+    static int setxattr_cb (const char *, const char *, const char *, size_t, int);
+    static int getxattr_cb (const char *, const char *, char *, size_t);
+    static int listxattr_cb (const char *, char *, size_t);
+    static int removexattr_cb (const char *, const char *);
+    static int opendir_cb (const char *, struct fuse_file_info *);
+    static int readdir_cb(const char *, void *, fuse_fill_dir_t, off_t, struct fuse_file_info *, enum fuse_readdir_flags);
+    static int releasedir_cb (const char *, struct fuse_file_info *);
+    static int fsyncdir_cb (const char *, int, struct fuse_file_info *);
+    static void *init_cb (struct fuse_conn_info *, struct fuse_config *);
+    static void destroy_cb (void *);
+    static int access_cb (const char *, int);
+    static int create_cb (const char *, mode_t, struct fuse_file_info *);
+    static int lock_cb (const char *, struct fuse_file_info *, int, struct flock *);
+    static int utimens_cb (const char *, const struct timespec tv[2], struct fuse_file_info *);
+    static int bmap_cb (const char *, size_t, uint64_t *);
+#if FUSE_USE_VERSION < 35
+    static int ioctl_cb (const char *, int, void *, struct fuse_file_info *, unsigned int, void *);
+#else
+    static int ioctl_cb (const char *, unsigned int, void *, struct fuse_file_info *, unsigned int, void *);
+#endif
+    static int poll_cb (const char *, struct fuse_file_info *, struct fuse_pollhandle *, unsigned *);
+    static int write_buf_cb (const char *, struct fuse_bufvec *, off_t, struct fuse_file_info *);
+    static int read_buf_cb (const char *, struct fuse_bufvec **, size_t, off_t, struct fuse_file_info *);
+    static int flock_cb (const char *, struct fuse_file_info *, int);
+    static int fallocate_cb (const char *, int, off_t, off_t, struct fuse_file_info *);
+    static ssize_t copy_file_range_cb (const char *, struct fuse_file_info *, off_t, const char *,
+                                       struct fuse_file_info *, off_t, size_t, int);
+    static off_t lseek_cb (const char *, off_t, int, struct fuse_file_info *);
 }; //class FusePP
 }; // namespace io::github::vuanhduy::libfusepp
 
--- src/FusePP.cpp
+++ src/FusePP.cpp
@@ -34,11 +34,47 @@ FusePP::FusePP() {
 
 void FusePP::load_operation() {
     s_operations.getattr = getattr_cb;
-    s_operations.readdir = readdir_cb;
-    s_operations.create = create_cb;
+    s_operations.readlink = readlink_cb;
+    s_operations.mknod = mknod_cb;
+    s_operations.mkdir = mkdir_cb;
+    s_operations.unlink = unlink_cb;
+    s_operations.rmdir = rmdir_cb;
+    s_operations.symlink = symlink_cb;
+    s_operations.rename = rename_cb;
+    s_operations.link = link_cb;
+    s_operations.chmod = chmod_cb;
+    s_operations.chown = chown_cb;
+    s_operations.truncate = truncate_cb;
     s_operations.open = open_cb;
     s_operations.read = read_cb;
     s_operations.write = write_cb;
+    s_operations.statfs = statfs_cb;
+    s_operations.flush = flush_cb;
+    s_operations.release = release_cb;
+    s_operations.fsync = fsync_cb;
+    s_operations.setxattr = setxattr_cb;
+    s_operations.getxattr = getxattr_cb;
+    s_operations.listxattr = listxattr_cb;
+    s_operations.removexattr = removexattr_cb;
+    s_operations.opendir = opendir_cb;
+    s_operations.readdir = readdir_cb;
+    s_operations.releasedir = releasedir_cb;
+    s_operations.fsyncdir = fsyncdir_cb;
+    s_operations.init = init_cb;
+    s_operations.destroy = destroy_cb;
+    s_operations.access = access_cb;
+    s_operations.create = create_cb;
+    s_operations.lock = lock_cb;
+    s_operations.utimens = utimens_cb;
+    s_operations.bmap = bmap_cb;
+    s_operations.ioctl = ioctl_cb;
+    s_operations.poll = poll_cb;
+    s_operations.write_buf = write_buf_cb;
+    s_operations.read_buf = read_buf_cb;
+    s_operations.flock = flock_cb;
+    s_operations.fallocate = fallocate_cb;
+    s_operations.copy_file_range = copy_file_range_cb;
+    s_operations.lseek = lseek_cb;
 }
 
 int FusePP::launch(int argc, char *argv[]) {
@@ -52,30 +88,215 @@ int FusePP::getattr_cb(const char *path, struct stat *buf, struct fuse_file_info
     return fuse->getattr(path, buf, fi);
 }
 
+int FusePP::readlink_cb (const char *path, char *buf, size_t size) {
+    GET_INSTANCE(fuse);
+    return fuse->readlink(path, buf, size);
+}
+
+int FusePP::mknod_cb (const char *path, mode_t mode, dev_t dev) {
+    GET_INSTANCE(fuse);
+    return fuse->mknod(path, mode, dev);
+}
+
+int FusePP::mkdir_cb (const char *path, mode_t mode) {
+    GET_INSTANCE(fuse);
+    return fuse->mkdir(path, mode);
+}
+
+int FusePP::unlink_cb (const char *path) {
+    GET_INSTANCE(fuse);
+    return fuse->unlink(path);
+}
+
+int FusePP::rmdir_cb (const char *path) {
+    GET_INSTANCE(fuse);
+    return fuse->rmdir(path);
+}
+
+int FusePP::symlink_cb (const char *target, const char *link) {
+    GET_INSTANCE(fuse);
+    return fuse->symlink(target, link);
+}
+
+int FusePP::rename_cb (const char *old_path, const char *new_path, unsigned int flags) {
+    GET_INSTANCE(fuse);
+    return fuse->rename(old_path, new_path, flags);
+}
+
+int FusePP::link_cb (const char *old_path, const char *new_path) {
+    GET_INSTANCE(fuse);
+    return fuse->link(old_path, new_path);
+}
+
+int FusePP::chmod_cb (const char *path, mode_t mode, struct fuse_file_info *fi) {
+    GET_INSTANCE(fuse);
+    return fuse->chmod(path, mode, fi);
+}
+
+int FusePP::chown_cb (const char *path, uid_t uid, gid_t gid, struct fuse_file_info *fi) {
+    GET_INSTANCE(fuse);
+    return fuse->chown(path, uid, gid, fi);
+}
+
+int FusePP::truncate_cb (const char *path, off_t off, struct fuse_file_info *fi) {
+    GET_INSTANCE(fuse);
+    return fuse->truncate(path, off, fi);
+}
+
+int FusePP::open_cb (const char *path, struct fuse_file_info *fi) {
+    GET_INSTANCE(fuse);
+    return fuse->open(path, fi);
+}
+
+int FusePP::read_cb (const char *path, char *buf, size_t size, off_t off, struct fuse_file_info *fi) {
+    GET_INSTANCE(fuse);
+    return fuse->read(path, buf, size, off, fi);
+}
+
+int FusePP::write_cb (const char *path, const char *buf, size_t size, off_t off, struct fuse_file_info *fi) {
+    GET_INSTANCE(fuse);
+    return fuse->write(path, buf, size, off, fi);
+}
+
+int FusePP::statfs_cb (const char *path, struct statvfs *buf) {
+    GET_INSTANCE(fuse);
+    return fuse->statfs(path, buf);
+}
+
+int FusePP::flush_cb (const char *path, struct fuse_file_info *fi) {
+    GET_INSTANCE(fuse);
+    return fuse->flush(path, fi);
+}
+
+int FusePP::release_cb (const char *path, struct fuse_file_info *fi) {
+    GET_INSTANCE(fuse);
+    return fuse->release(path, fi);
+}
+
+int FusePP::fsync_cb (const char *path, int datasync, struct fuse_file_info *fi) {
+    GET_INSTANCE(fuse);
+    return fuse->fsync(path, datasync, fi);
+}
+
+int FusePP::setxattr_cb (const char *path, const char *name, const char *value, size_t size, int flags) {
+    GET_INSTANCE(fuse);
+    return fuse->setxattr(path, name, value, size, flags);
+}
+
+int FusePP::getxattr_cb (const char *path, const char *name, char *value, size_t size) {
+    GET_INSTANCE(fuse);
+    return fuse->getxattr(path, name, value, size);
+}
+
+int FusePP::listxattr_cb (const char *path, char *list, size_t size) {
+    GET_INSTANCE(fuse);
+    return fuse->listxattr(path, list, size);
+}
+
+int FusePP::removexattr_cb (const char *path, const char *name) {
+    GET_INSTANCE(fuse);
+    return fuse->removexattr(path, name);
+}
+
+int FusePP::opendir_cb (const char *path, struct fuse_file_info *fi) {
+    GET_INSTANCE(fuse);
+    return fuse->opendir(path, fi);
+}
+
 int FusePP::readdir_cb(const char *path, void *buf, fuse_fill_dir_t filler, off_t off, struct fuse_file_info *fi,
                        enum fuse_readdir_flags flags) {
     GET_INSTANCE(fuse);
     return fuse->readdir(path, buf, filler, off, fi, flags);
 }
 
-int FusePP::create_cb(const char *path, mode_t mode, struct fuse_file_info *fi) {
+int FusePP::releasedir_cb (const char *path, struct fuse_file_info *fi) {
+    GET_INSTANCE(fuse);
+    return fuse-> releasedir(path, fi);
+}
+int FusePP::fsyncdir_cb (const char *path, int datasync, struct fuse_file_info *fi) {
+    GET_INSTANCE(fuse);
+    return fuse->fsyncdir(path, datasync, fi);
+}
+
+void *FusePP::init_cb (struct fuse_conn_info *fi, struct fuse_config *cf) {
+    GET_INSTANCE(fuse);
+    return fuse->init(fi, cf);
+}
+
+void FusePP::destroy_cb (void *private_data) {
+    GET_INSTANCE(fuse);
+    return fuse->destroy(private_data);
+}
+
+int FusePP::access_cb (const char *path, int mask) {
+    GET_INSTANCE(fuse);
+    return fuse->access(path, mask);
+}
+
+int FusePP::create_cb (const char *path, mode_t mode, struct fuse_file_info *fi) {
     GET_INSTANCE(fuse);
     return fuse->create(path, mode, fi);
 }
 
-int FusePP::open_cb(const char *path, struct fuse_file_info *fi) {
+int FusePP::lock_cb (const char *path, struct fuse_file_info *fi, int cmd, struct flock *lock) {
     GET_INSTANCE(fuse);
-    return fuse->open(path, fi);
+    return fuse->lock(path, fi, cmd, lock);
 }
 
-int FusePP::read_cb(const char *path, char *buf, size_t size, off_t off, fuse_file_info *fi) {
+int FusePP::utimens_cb (const char *path, const struct timespec tv[2], struct fuse_file_info *fi) {
     GET_INSTANCE(fuse);
-    return fuse->read(path, buf, size, off, fi);
+    return fuse->utimens(path, tv, fi);
 }
 
-int FusePP::write_cb(const char *path, const char *buf, size_t size, off_t off, struct fuse_file_info *fi) {
+int FusePP::bmap_cb (const char *path, size_t blocksize, uint64_t *idx) {
     GET_INSTANCE(fuse);
-    return fuse->write(path, buf, size, off, fi);
+    return fuse->bmap(path, blocksize, idx);
+}
+
+#if FUSE_USE_VERSION < 35
+int FusePP::ioctl_cb (const char *path, int cmd, void *arg, struct fuse_file_info *fi, unsigned int flags, void *data) {
+#else
+int FusePP::ioctl_cb (const char *path, unsigned int cmd, void *arg, struct fuse_file_info *fi, unsigned int flags, void *data) {
+#endif
+    GET_INSTANCE(fuse);
+    return fuse->ioctl(path, cmd, arg, fi, flags, data);
+}
+
+int FusePP::poll_cb (const char *path, struct fuse_file_info *fi, struct fuse_pollhandle *ph, unsigned *reventsp) {
+    GET_INSTANCE(fuse);
+    return fuse->poll(path, fi, ph, reventsp);
+}
+
+int FusePP::write_buf_cb (const char *path, struct fuse_bufvec *buf, off_t off, struct fuse_file_info *fi) {
+    GET_INSTANCE(fuse);
+    return fuse->write_buf(path, buf, off, fi);
+}
+
+int FusePP::read_buf_cb (const char *path, struct fuse_bufvec **buf, size_t size, off_t off,
+                         struct fuse_file_info *fi) {
+    GET_INSTANCE(fuse);
+    return fuse->read_buf(path, buf, size, off, fi);
+}
+
+int FusePP::flock_cb (const char *path, struct fuse_file_info *fi, int op) {
+    GET_INSTANCE(fuse);
+    return fuse->flock(path, fi, op);
+}
+
+int FusePP::fallocate_cb (const char *path, int mode, off_t off, off_t length, struct fuse_file_info *fi) {
+    GET_INSTANCE(fuse);
+    return fuse->fallocate(path, mode, off, length, fi);
+}
+
+ssize_t FusePP::copy_file_range_cb (const char *path_in, struct fuse_file_info *fi_in, off_t off_in, const char *path_out,
+                                    struct fuse_file_info *fi_out, off_t off_out, size_t size, int flags) {
+    GET_INSTANCE(fuse);
+    return fuse->copy_file_range(path_in, fi_in, off_in, path_out, fi_out, off_out, size, flags);
+}
+
+off_t FusePP::lseek_cb (const char *path, off_t off, int whence, struct fuse_file_info *fi) {
+    GET_INSTANCE(fuse);
+    return fuse->lseek_cb(path, off, whence, fi);
 }
 
 #undef GET_INSTANCE
